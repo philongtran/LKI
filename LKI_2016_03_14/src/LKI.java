9c8a825c80c006936a6621fed518ca1b47b7844f
Phi Long Tran <philongtran@outlook.de>
2016-03-15 16:49:18 +0100

    add evaluate method

--- LKI_2016_03_14/src/LKI.java
+++ LKI_2016_03_14/src/LKI.java
@@ -1,4 +1,39 @@
 
 public class LKI {
 
+	public static void main(String[] args) {
+		System.out.println(evaluatea(true, true, true, true));
+		System.out.println(evaluatea(true, true, true, false));
+		System.out.println(evaluatea(true, true, false, true));
+		System.out.println(evaluatea(true, true, false, false));
+		System.out.println(evaluatea(true, false, true, true));
+		System.out.println(evaluatea(true, false, true, false));
+		System.out.println(evaluatea(true, false, false, true));
+		System.out.println(evaluatea(true, false, false, false));
+		System.out.println(evaluatea(false, true, true, true));
+		System.out.println(evaluatea(false, true, true, false));
+		System.out.println(evaluatea(false, true, false, true));
+		System.out.println(evaluatea(false, true, false, false));
+		System.out.println(evaluatea(false, false, true, true));
+		System.out.println(evaluatea(false, false, true, false));
+		System.out.println(evaluatea(false, false, false, true));
+		System.out.println(evaluatea(false, false, false, false));
+	}
+
+	public static boolean imp(boolean x, boolean y) {
+		return !x || y;
+	}
+
+	public static boolean equ(boolean x, boolean y) {
+		return imp(x, y) && imp(y, x);
+	}
+
+	public static boolean evaluatea(boolean a, boolean b, boolean c, boolean d) {
+		return imp(imp(a, b) && imp(b, c) && imp(c, d) && imp(d, a), equ(a, c));
+	}
+
+	public static boolean evaluateb(boolean a, boolean b, boolean c, boolean d) {
+		return imp(a || b || c || d, imp(!a && !b, !d || c));
+	}
+
 }
