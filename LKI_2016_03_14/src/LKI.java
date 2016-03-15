43eae3e80cff17287c3a0927b78e521e1138cec2
Phi Long Tran <philongtran@outlook.de>
2016-03-15 16:54:08 +0100

    add all valid combinations

--- LKI_2016_03_14/src/LKI.java
+++ LKI_2016_03_14/src/LKI.java
@@ -2,6 +2,7 @@
 public class LKI {
 
 	public static void main(String[] args) {
+		System.out.println("AB C D");
 		System.out.println(evaluatea(true, true, true, true));
 		System.out.println(evaluatea(true, true, true, false));
 		System.out.println(evaluatea(true, true, false, true));
@@ -18,6 +19,24 @@ public class LKI {
 		System.out.println(evaluatea(false, false, true, false));
 		System.out.println(evaluatea(false, false, false, true));
 		System.out.println(evaluatea(false, false, false, false));
+
+		System.out.println(evaluateb(true, true, true, true));
+		System.out.println(evaluateb(true, true, true, false));
+		System.out.println(evaluateb(true, true, false, true));
+		System.out.println(evaluateb(true, true, false, false));
+		System.out.println(evaluateb(true, false, true, true));
+		System.out.println(evaluateb(true, false, true, false));
+		System.out.println(evaluateb(true, false, false, true));
+		System.out.println(evaluateb(true, false, false, false));
+		System.out.println(evaluateb(false, true, true, true));
+		System.out.println(evaluateb(false, true, true, false));
+		System.out.println(evaluateb(false, true, false, true));
+		System.out.println(evaluateb(false, true, false, false));
+		System.out.println(evaluateb(false, false, true, true));
+		System.out.println(evaluateb(false, false, true, false));
+		System.out.println(evaluateb(false, false, false, true));
+		System.out.println(evaluateb(false, false, false, false));
+
 	}
 
 	public static boolean imp(boolean x, boolean y) {
