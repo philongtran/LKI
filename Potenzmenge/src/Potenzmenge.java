3b79a8fe5138310c2ec1fbec48c412902306b3ae
philo <philo@Long>
2016-04-18 06:56:36 +0200

    add comments

--- Potenzmenge/src/Potenzmenge.java
+++ Potenzmenge/src/Potenzmenge.java
@@ -7,6 +7,7 @@
 public class Potenzmenge {
 
 	public static void main(String[] args) {
+		// number of elements in the menge (change to other number if needed)
 		int elements = 5;
 		int[] menge = new int[elements];
 		String binary;
@@ -15,26 +16,32 @@ public class Potenzmenge {
 		String mengeString = "M = {";
 		char bi;
 
+		// fill menge with numbers and output it
 		for (int i = 0; i < elements; i++) {
 			menge[i] = i + 1;
 			mengeString = mengeString + " " + menge[i] + ",";
 		}
+		// cut of the last ","
 		mengeString = mengeString.substring(0, mengeString.length() - 1);
 		System.out.println(mengeString + " }");
 
+		// create binary numbers
 		for (int i = 0; i < Math.pow(2, elements); i++) {
 			binary = Integer.toBinaryString(i);
 			while (binary.length() < elements) {
+				// add zeros in front for consistent look
 				binary = "0" + binary;
 			}
 			System.out.println(binary);
-			for (int a = 0; a < elements; a++) {
-				bi = binary.charAt(a);
+			// convert binary number to a number for the menge
+			for (int index = 0; index < elements; index++) {
+				bi = binary.charAt(index);
 				if (Character.getNumericValue(bi) == 1) {
-					result = result + " " + menge[a] + ",";
+					result = result + " " + menge[index] + ",";
 				}
 			}
 			if (result.length() > 2)
+				// cut of the last ","
 				result = result.substring(0, result.length() - 1);
 			potenzmenge = potenzmenge + result + " })";
 			System.out.println(potenzmenge);
